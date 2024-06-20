9ae9928cab7cf17c2c6371b1c16a7edfd8aa8c83
rvadimv <razvodov.vadim@gmail.com>
2024-06-21 00:56:34 +0300

    синтаксис

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ function App() {
     return (
         <div className="App">
 
+            <Menu>
+                <ul>
+                    <li><a href="#">menu item 1</a></li>
+                    <li><a href="#">menu item 2</a></li>
+                    <li><a href="#">menu item 3</a></li>
+                </ul>
+            </Menu>
+
             <Box>
                 <StyleBtn as={"a"} href={"#"}>Link</StyleBtn>
                 <StyleBtn as={StyleLink} href={"#"}>LinkComponenet</StyleBtn>
@@ -28,6 +36,14 @@ const StyleBtn = styled.button`
     color: aliceblue;
     font-size: 2rem;
     font-weight: bold;
+
+    &:hover {
+        background-color: brown;
+    }
+
+    &:last-child {
+        background-color: darkslateblue;
+    }
 `
 
 const StyleLink = styled.a`
@@ -49,5 +65,29 @@ const Box = styled.div`
     justify-content: center;
     align-items: center;
     gap: 20px;
+
+    button {
+        cursor: pointer;
+    }
+
+    ${StyleLink} {
+        cursor: zoom-in;
+    }
+`
+
+const Menu = styled.nav`
+    ul {
+        list-style: none;
+        padding: 0;
+        display: flex;
+
+        li > a {
+            color: #264706;
+        }
+
+        li + li {
+            margin-left: 20px;
+        }
+    }
 `
 
