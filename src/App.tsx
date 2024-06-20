7eec582f1a8ed7a32f41a615c55bd0added1de94
rvadimv <razvodov.vadim@gmail.com>
2024-06-20 23:37:24 +0300

    inside App.tsx styledComponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                Hello
-            </header>
+
+            <Box>
+                <StyleBtn>Hello</StyleBtn>
+            </Box>
+
+
         </div>
     );
 }
 
 export default App;
+
+const StyleBtn = styled.button`
+    border: none;
+    background-color: blueviolet;
+    padding: 10px 20px;
+    border-radius: 5px;
+    color: aliceblue;
+    font-size: 2rem;
+    font-weight: bold;
+`
+const Box = styled.div`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`
