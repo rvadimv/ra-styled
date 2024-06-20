4f50aafe058da19ed8f69a218e7389d5784f8d6d
rvadimv <razvodov.vadim@gmail.com>
2024-06-21 01:28:05 +0300

    components folder

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ function App() {
 
             <Menu>
                 <ul>
-                    <li><a href="#">menu item 1</a></li>
-                    <li><a href="#">menu item 2</a></li>
-                    <li><a href="#">menu item 3</a></li>
+                    <li><a href="">menu item 1</a></li>
+                    <li><a href="">menu item 2</a></li>
+                    <li><a href="">menu item 3</a></li>
                 </ul>
             </Menu>
 
