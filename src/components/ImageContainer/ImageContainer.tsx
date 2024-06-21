79a11583c7e8500f030cbf6187124931665377c3
rvadimv <razvodov.vadim@gmail.com>
2024-06-21 17:11:27 +0300

    Reapply "try revert"
    This reverts commit f82c61dfd65d7e6600906485155d89bd7b22cdde.

---
+++ src/components/ImageContainer/ImageContainer.tsx
@@ -0,0 +1,13 @@
+
+export const ImageContainer = () =>{
+    return(
+    <>
+        <p>Hello</p>
+        <p>Hello</p>
+        <p>Hello</p>
+        <p>Hello</p>
+        <p>Hello</p>
+        <p>Hello</p>
+    </>
+)
+}
